62d60bee24cc80186e8fe70e9a17ee550d43e440
余文君 <julyyu@bchandeiMac.local>
2015-08-11 14:10:22 +0800

    Initial Commit

---
+++ edrs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  edrs
+//
+//  Created by 余文君, July on 15/8/11.
+//  Copyright (c) 2015年 julyyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ edrs/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  edrs
+//
+//  Created by 余文君, July on 15/8/11.
+//  Copyright (c) 2015年 julyyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
